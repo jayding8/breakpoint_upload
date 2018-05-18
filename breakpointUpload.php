c35c293d9ed004291ae8f4e0499b30989a6bfc90
JayDing <dingzhengjie@hoge.cn>
2018-05-18 19:12:19 +0800

    断点续传js优化

--- breakpointUpload.php
+++ breakpointUpload.php
@@ -5,20 +5,21 @@
  * Date: 2018/4/16
  * Time: 上午9:36
  */
-$dir=$_POST['filename'];
-$dir="uploads/".md5($dir);
+$dir=$_POST['fileName'];
+$dir="uploads/".md5($dir).date('His');
 file_exists($dir) or mkdir($dir,0777,true);
 $path=$dir."/".$_POST['blob'];
 move_uploaded_file($_FILES["file"]["tmp_name"],$path);
-if(isset($_POST['lastone'])){
-    $count=$_POST['lastone'];
-    $fp   = fopen($_POST['filename'],"abw");
-    for($i=0;$i<=$count;$i++){
+if(isset($_POST['lastOne'])){
+    $count=$_POST['lastOne'];
+    $fp   = fopen($_POST['fileName'],"abw");
+    for($i=0;$i<$count;$i++){
         $handle = fopen($dir."/".$i,"rb");
         fwrite($fp,fread($handle,filesize($dir."/".$i)));
         fclose($handle);
     }
     fclose($fp);
+    echo 111;
 }
 
 ?>
